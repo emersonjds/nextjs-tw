3e06761917f06f54507737f9cbfcc0024a18e984
emersonjds <emerson_jdss@hotmail.com>
2024-01-19 17:21:21 -0300

    add google fonts

--- src/app/typography/page.tsx
+++ src/app/typography/page.tsx
@@ -1,7 +1,10 @@
 export default function Typhografy() {
   return (
     <div>
-      <h1>Texto 1</h1>
+      <h1 className="text-2xl underline decoration-red-500 outline-double">
+        Texto 1
+      </h1>
+
       <h2>Texto 2</h2>
       <h3>Texto 3</h3>
       <h4 className="text-base">Texto 4</h4>
