d5f0ea55472763ec65e34e48e72e33044bc5a3fa
emersonjds <emerson_jdss@hotmail.com>
2024-01-19 15:28:11 -0300

    apply layers to understand

---
+++ src/app/typography/page.tsx
@@ -0,0 +1,14 @@
+export default function Typhografy() {
+  return (
+    <div>
+      <h1 className="text-4xl">Hello world!</h1>
+      <h2 className="text-3xl">Hello world!</h2>
+      <h3 className="text-2xl">Hello world!</h3>
+      <h4 className="text-xl">Hello world!</h4>
+      <h5 className="text-lg">Hello world!</h5>
+      <h6 className="text-base">Hello world!</h6>
+      <p className="text-sm">Hello world!</p>
+      <p className="text-xs">Hello world!</p>
+    </div>
+  );
+}
