20a35f79a68b50f93fb4209c1f5f6e20f5e00efa
emersonjds <emerson_jdss@hotmail.com>
2024-01-20 16:43:59 -0300

    testes with flex

--- src/app/flex/page.tsx
+++ src/app/flex/page.tsx
@@ -1,7 +1,17 @@
+import styles from "./Flex.module.css";
+
 export default function Flex() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <>
+      <div className="flex h-screen w-full p-5 bg-black gap-2">
+        <div className="h-1/2 w-1/2  border-solid border-4 flex items-center justify-center">
+          <h1 className="text-bold size-5">1</h1>
+        </div>
+
+        <div className="h-1/2 w-1/2 border-solid border-4">2</div>
+
+        <div className="h-1/2 w-1/2 border-solid border-4">3</div>
+      </div>
+    </>
   );
 }
