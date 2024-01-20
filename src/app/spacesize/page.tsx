890c3305c48f75d22a5dc65acd15e198b1dd30f9
emersonjds <emerson_jdss@hotmail.com>
2024-01-19 21:38:09 -0300

    spacings

---
+++ src/app/spacesize/page.tsx
@@ -0,0 +1,12 @@
+export default function SpaceSize() {
+  return (
+    <>
+      <div className="bg-red-500 m-4">
+        <p>Box with margin</p>
+      </div>
+      <div className="bg-blue-600 p-4">
+        <p>Box with padidng</p>
+      </div>
+    </>
+  );
+}
