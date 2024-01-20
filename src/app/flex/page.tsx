d6ae5feee1420e916ab676b981ce8042ca0a6b11
emersonjds <emerson_jdss@hotmail.com>
2024-01-20 16:54:36 -0300

    flex-reverse

--- src/app/flex/page.tsx
+++ src/app/flex/page.tsx
@@ -3,14 +3,14 @@ import styles from "./Flex.module.css";
 export default function Flex() {
   return (
     <>
-      <div className="flex h-screen w-full p-5 bg-black gap-2">
-        <div className="h-1/2 w-1/2  border-solid border-4 flex items-center justify-center">
+      <div className="flex h-screen w-full p-5 bg-black gap-2 flex-row-reverse">
+        <div className="h-1/2 w-1/2  bg-red-500 border-4 flex ">
           <h1 className="text-bold size-5">1</h1>
         </div>
 
-        <div className="h-1/2 w-1/2 border-solid border-4">2</div>
+        <div className="h-1/2 w-1/2 bg-green-500 border-4">2</div>
 
-        <div className="h-1/2 w-1/2 border-solid border-4">3</div>
+        <div className="h-1/2 w-1/2 bg-yellow-400 border-4">3</div>
       </div>
     </>
   );
