45c402b8190526008c9554c8f8c506f5a31e4ff1
emersonjds <emerson_jds@live.com>
2024-01-20 22:29:07 -0300

    adjust grid with breakpoints

--- src/app/grid/page.tsx
+++ src/app/grid/page.tsx
@@ -1,6 +1,6 @@
 export default function Grid() {
   return (
-    <div className="grid grid-cols-4 gap-2 h-screen w-full p-4 bg-black md:grid-cols-4 lg:grid-cols-3">
+    <div className="grid grid-cols-2 gap-2 h-screen w-full p-4 bg-black md:grid-cols-3 lg:grid-cols-4">
       <div className="bg-blue-500 p-3">1</div>
       <div className="bg-blue-500 p-3">2</div>
       <div className="bg-blue-500 p-3">3</div>
