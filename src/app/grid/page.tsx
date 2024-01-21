9ff7837307cb28f87ce84dc75c49bca8ce5ab243
emersonjds <emerson_jds@live.com>
2024-01-20 22:36:52 -0300

    adjust erros with container

--- src/app/grid/page.tsx
+++ src/app/grid/page.tsx
@@ -1,11 +1,20 @@
 export default function Grid() {
   return (
-    <div className="grid grid-cols-2 gap-2 h-screen w-full p-4 bg-black md:grid-cols-3 lg:grid-cols-4">
-      <div className="bg-blue-500 p-3">1</div>
-      <div className="bg-blue-500 p-3">2</div>
-      <div className="bg-blue-500 p-3">3</div>
-      <div className="bg-blue-500 p-3">4</div>
-      <div className="bg-blue-500 p-3">5</div>
+
+
+    <div className="w-screen h-screen">
+      <div className="grid grid-cols-2 gap-2 h-screen p-4 bg-black md:grid-cols-3 lg:grid-cols-4">
+        <div className="bg-blue-500 p-3">1</div>
+        <div className="bg-blue-500 p-3">2</div>
+        <div className="bg-blue-500 p-3">3</div>
+        <div className="bg-blue-500 p-3">4</div>
+        <div className="bg-blue-500 p-3">5</div>
+      </div>
     </div>
+
+
+
+
+
   );
 }
