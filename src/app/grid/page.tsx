520b2231232c5f82a386e4e957cb1d9b73c73258
emersonjds <emerson_jdss@hotmail.com>
2024-01-27 23:22:32 -0300

    add grids

--- src/app/grid/page.tsx
+++ src/app/grid/page.tsx
@@ -1,20 +1,38 @@
 export default function Grid() {
   return (
-
-
     <div className="w-screen h-screen">
-      <div className="grid grid-cols-2 gap-2 h-screen p-4 bg-black md:grid-cols-3 lg:grid-cols-4">
-        <div className="bg-blue-500 p-3">1</div>
-        <div className="bg-blue-500 p-3">2</div>
-        <div className="bg-blue-500 p-3">3</div>
-        <div className="bg-blue-500 p-3">4</div>
-        <div className="bg-blue-500 p-3">5</div>
+      <div className="grid grid-cols-4 gap-4 p-5">
+        <div className="p-4 bg-blue-500">
+          1
+        </div>
+        <div className="p-4 bg-blue-500">
+          1
+        </div>
+        <div className="p-4 bg-blue-500">
+          1
+        </div>
+        <div className="p-4 bg-blue-500">
+          1
+        </div>
+        <div className="p-4 bg-blue-500">
+          1
+        </div>
+        <div className="p-4 bg-blue-500">
+          1
+        </div>
+        <div className="p-4 bg-blue-500">
+          1
+        </div>
+        <div className="p-4 bg-blue-500">
+          1
+        </div>
+        <div className="p-4 bg-blue-500">
+          1
+        </div>
+        <div className="p-4 bg-blue-500">
+          1
+        </div>
       </div>
     </div>
-
-
-
-
-
   );
 }
