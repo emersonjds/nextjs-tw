1cf770a3c262a011a38af3155bf6c0f46b429296
emersonjds <emerson_jdss@hotmail.com>
2024-01-29 22:42:37 -0300

    undertanding container

--- src/app/grid/page.tsx
+++ src/app/grid/page.tsx
@@ -1,20 +1,22 @@
 export default function Grid() {
   return (
-    <div className="w-screen h-screen">
-      <div className="grid grid-cols-4 gap-4 p-5">
-        <div className="p-4 bg-blue-500 col-span-2">
+    <div className="h-screen bg-black p-4">
+      <div className="container p-2 mx-auto">
+        <div className="grid grid-cols-4 gap-4 p-5">
+          <div className="p-4 bg-blue-500 col-span-2">
+            1
+          </div>
+          <div className="p-4 bg-blue-500">
+            1
+          </div>
+          <div className="p-4 bg-blue-500">
+            1
+          </div>
+          <div className="p-4 bg-blue-500">
+            1
+          </div>
           1
         </div>
-        <div className="p-4 bg-blue-500">
-          1
-        </div>
-        <div className="p-4 bg-blue-500">
-          1
-        </div>
-        <div className="p-4 bg-blue-500">
-          1
-        </div>
-        1
       </div>
     </div>
   );
