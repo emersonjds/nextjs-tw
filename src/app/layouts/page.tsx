823b5c1a8a83c9f3d23ecec254935e943adfdb7f
Emerson Silva <emerson_jdss@hotmail.com>
2024-02-18 15:29:55 -0300

    adjust proportion of height

--- src/app/layouts/page.tsx
+++ src/app/layouts/page.tsx
@@ -1,7 +1,7 @@
 export default function Layout() {
   return (
     <div className="Parent h-screen bg-slate-900 text-white">
-      <div className="container mx-auto bg-red-500">
+      <div className="container mx-auto h-screen bg-red-500">
         <p>Hello</p>
       </div>
     </div>
