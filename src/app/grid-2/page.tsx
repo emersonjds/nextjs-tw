fb60e6b0a51b16d511ce9a18c6614f7635d099de
Emerson Silva <emerson_jdss@hotmail.com>
2024-02-18 15:10:26 -0300

    add row-span and col-span

--- src/app/grid-2/page.tsx
+++ src/app/grid-2/page.tsx
@@ -3,11 +3,11 @@ export default function Grid2() {
     <div className="Parent h-screen bg-slate-900 p-4 text-white">
       <div className="container mx-auto">
         <div className="grid grid-flow-row-dense grid-cols-4 grid-rows-4 gap-4">
-          <div className="rounded-lg bg-slate-700 p-4">1</div>
+          <div className="col-span-2 rounded-lg bg-slate-700 p-4">1</div>
           <div className="rounded-lg bg-slate-700 p-4">2</div>
           <div className="rounded-lg bg-slate-700 p-4">3</div>
           <div className="rounded-lg bg-slate-700 p-4">4</div>
-          <div className="rounded-lg bg-slate-700 p-4">5</div>
+          <div className="row-span-3 rounded-lg bg-slate-700 p-4">5</div>
           <div className="rounded-lg bg-slate-700 p-4">6</div>
           <div className="rounded-lg bg-slate-700 p-4">7</div>
           <div className="rounded-lg bg-slate-700 p-4">8</div>
