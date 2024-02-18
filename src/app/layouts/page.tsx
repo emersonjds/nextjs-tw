f81e3f1e506619b2455d83256601facb4f74ccc7
Emerson Silva <emerson_jdss@hotmail.com>
2024-02-18 15:37:17 -0300

    add columns

--- src/app/layouts/page.tsx
+++ src/app/layouts/page.tsx
@@ -1,8 +1,16 @@
 export default function Layout() {
   return (
     <div className="Parent h-screen bg-slate-900 text-white">
-      <div className="container mx-auto h-screen bg-red-500">
-        <p>Hello</p>
+      <div className="container mx-auto h-screen bg-red-500 columns-3">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt voluptates voluptatibus possimus maxime unde nulla, cumque quam id nam inventore illo fugit fugiat? Ipsam quaerat doloribus beatae? At, saepe beatae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quam minima laudantium deserunt labore quidem ex qui, eum culpa porro nesciunt dignissimos, modi hic consequuntur possimus at, reiciendis accusamus velit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam consequatur velit ducimus vel cumque doloribus dicta quam nulla, placeat magni. Recusandae optio, culpa voluptatum obcaecati molestiae atque inventore doloremque laboriosam?
+        </p>
       </div>
     </div>
   );
