c3cc615745c6715ff782465a6b96473ee7ecd3c9
Emerson Silva <emerson_jdss@hotmail.com>
2024-02-18 15:13:54 -0300

    add bg color

--- src/app/layouts/page.tsx
+++ src/app/layouts/page.tsx
@@ -1,7 +1,7 @@
 export default function Layout() {
   return (
     <div className="Parent h-screen bg-slate-900 text-white">
-      <div className="container">
+      <div className="container bg-red-500">
         <p>Hello</p>
       </div>
     </div>
