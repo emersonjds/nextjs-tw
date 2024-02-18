6309ce3b73de9d8f2fcda319c9c3d7098c4a7bb4
Emerson Silva <emerson_jdss@hotmail.com>
2024-02-18 15:32:54 -0300

    adjust center of container in theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,38 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-
-      
-
-      screens:{
-        sm: "480px",
-        md: "768px",
-        lg: "976px",
-        xl: "1440px",
+      container: {
+        center: true,
       },
 
-      
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
 
       colors: {
-        vuejs: "#41B883",
+        vuejs: '#41B883',
         primary: {
-          100: "#F0F4FF",
-          200: "#D9E2FF",
-          300: "#A6BCFF",
-          400: "#598BFF",
-          500: "#3366FF",
-          600: "#274BDB",
-          700: "#1A34B8",
-          800: "#102694",
-          900: "#091C7A",
+          100: '#F0F4FF',
+          200: '#D9E2FF',
+          300: '#A6BCFF',
+          400: '#598BFF',
+          500: '#3366FF',
+          600: '#274BDB',
+          700: '#1A34B8',
+          800: '#102694',
+          900: '#091C7A',
         },
-      }
+      },
     },
   },
   plugins: [],
