c4a35ef2115c38fb8544c9bdbfad2c4abea83549
Emerson Silva <emerson_jdss@hotmail.com>
2024-02-18 15:02:47 -0300

    changes on layout

--- src/app/grid-2/page.tsx
+++ src/app/grid-2/page.tsx
@@ -1,23 +1,23 @@
 export default function Grid2() {
   return (
-    <div className="Parent h-screen bg-slate-900 text-white">
+    <div className="Parent h-screen bg-slate-900 p-4 text-white">
       <div className="container mx-auto">
-        <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-3 gap-4">
-          <div className="bg-slate-700 p-4">1</div>
-          <div className="bg-slate-700 p-4">2</div>
-          <div className="bg-slate-700 p-4">3</div>
-          <div className="bg-slate-700 p-4">4</div>
-          <div className="bg-slate-700 p-4">5</div>
-          <div className="bg-slate-700 p-4">6</div>
-          <div className="bg-slate-700 p-4">7</div>
-          <div className="bg-slate-700 p-4">8</div>
-          <div className="bg-slate-700 p-4">9</div>
-          <div className="bg-slate-700 p-4">7</div>
-          <div className="bg-slate-700 p-4">8</div>
-          <div className="bg-slate-700 p-4">9</div>
-          <div className="bg-slate-700 p-4">7</div>
-          <div className="bg-slate-700 p-4">8</div>
-          <div className="bg-slate-700 p-4">9</div>
+        <div className="grid grid-flow-row-dense grid-cols-4 grid-rows-4 gap-4">
+          <div className="rounded-lg bg-slate-700 p-4">1</div>
+          <div className="rounded-lg bg-slate-700 p-4">2</div>
+          <div className="rounded-lg bg-slate-700 p-4">3</div>
+          <div className="rounded-lg bg-slate-700 p-4">4</div>
+          <div className="rounded-lg bg-slate-700 p-4">5</div>
+          <div className="rounded-lg bg-slate-700 p-4">6</div>
+          <div className="rounded-lg bg-slate-700 p-4">7</div>
+          <div className="rounded-lg bg-slate-700 p-4">8</div>
+          <div className="rounded-lg bg-slate-700 p-4">9</div>
+          <div className="rounded-lg bg-slate-700 p-4">7</div>
+          <div className="rounded-lg bg-slate-700 p-4">8</div>
+          <div className="rounded-lg bg-slate-700 p-4">9</div>
+          <div className="rounded-lg bg-slate-700 p-4">7</div>
+          <div className="rounded-lg bg-slate-700 p-4">8</div>
+          <div className="rounded-lg bg-slate-700 p-4">9</div>
         </div>
       </div>
     </div>
