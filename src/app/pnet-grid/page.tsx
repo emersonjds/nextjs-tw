f7e96f0bf30bc14a53ec7d500c3a65a303ca4af7
Emerson Silva <emerson_jdss@hotmail.com>
2024-03-03 19:26:13 -0300

    changes on indent

--- src/app/pnet-grid/page.tsx
+++ src/app/pnet-grid/page.tsx
@@ -1,12 +1,12 @@
 export default function PnetGrid() {
-    return (
-        <div className="h-screen w-screen p-4 bg-black">
-            <div className="bg-green-500 h-1/6">Header</div>
-            <div className="grid md:grid-cols-2 lg:grid-cols-2 h-4/6">
-                <div className="bg-blue-500">Esquerda</div>
-                <div className="bg-yellow-500">Direita</div>
-            </div>
-            <div className="bg-red-500 h-1/6">Footer</div>
-        </div>
-    )
+  return (
+    <div className="h-screen w-screen p-4 bg-black">
+      <div className="bg-green-500 h-1/6">Header</div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-2 h-4/6">
+        <div className="bg-blue-500">Esquerda</div>
+        <div className="bg-yellow-500">Direita</div>
+      </div>
+      <div className="bg-red-500 h-1/6">Footer</div>
+    </div>
+  );
 }
