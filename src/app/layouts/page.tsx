1883a1b9dde48b182c1b60b977f9547d2cffdc57
Emerson Silva <emerson_jdss@hotmail.com>
2024-03-02 22:30:37 -0300

    remove p

--- src/app/layouts/page.tsx
+++ src/app/layouts/page.tsx
@@ -7,18 +7,6 @@ export default function Layout() {
           alt="tornado"
         />
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt
-          voluptates voluptatibus possimus maxime unde nulla, cumque quam id nam
-          inventore illo fugit fugiat? Ipsam quaerat doloribus beatae? At, saepe
-          beatae.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-          quam minima laudantium deserunt labore quidem ex qui, eum culpa porro
-          nesciunt dignissimos, modi hic consequuntur possimus at, reiciendis
-          accusamus velit.
-        </p>
-        <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam
           consequatur velit ducimus vel cumque doloribus dicta quam nulla,
           placeat magni. Recusandae optio, culpa voluptatum obcaecati molestiae
