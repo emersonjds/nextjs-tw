a30e18b3e442e438b88af6b5820854b1a6b3da6f
Emerson Silva <emerson_jdss@hotmail.com>
2024-03-02 22:30:21 -0300

    changes on code

--- src/app/layouts/page.tsx
+++ src/app/layouts/page.tsx
@@ -2,17 +2,27 @@ export default function Layout() {
   return (
     <div className="Parent h-screen bg-slate-900 text-white">
       <div className="container  bg-red-500 columns-sm	 h-screen">
-
-        <img src="https://super.abril.com.br/wp-content/uploads/2017/06/qual-a-diferenc3a7a-entre-tufc3a3o-tornado-furacc3a3o-e-ciclone.png" alt="tornado" />
-
+        <img
+          src="https://super.abril.com.br/wp-content/uploads/2017/06/qual-a-diferenc3a7a-entre-tufc3a3o-tornado-furacc3a3o-e-ciclone.png"
+          alt="tornado"
+        />
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt voluptates voluptatibus possimus maxime unde nulla, cumque quam id nam inventore illo fugit fugiat? Ipsam quaerat doloribus beatae? At, saepe beatae.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt
+          voluptates voluptatibus possimus maxime unde nulla, cumque quam id nam
+          inventore illo fugit fugiat? Ipsam quaerat doloribus beatae? At, saepe
+          beatae.
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quam minima laudantium deserunt labore quidem ex qui, eum culpa porro nesciunt dignissimos, modi hic consequuntur possimus at, reiciendis accusamus velit.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          quam minima laudantium deserunt labore quidem ex qui, eum culpa porro
+          nesciunt dignissimos, modi hic consequuntur possimus at, reiciendis
+          accusamus velit.
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam consequatur velit ducimus vel cumque doloribus dicta quam nulla, placeat magni. Recusandae optio, culpa voluptatum obcaecati molestiae atque inventore doloremque laboriosam?
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam
+          consequatur velit ducimus vel cumque doloribus dicta quam nulla,
+          placeat magni. Recusandae optio, culpa voluptatum obcaecati molestiae
+          atque inventore doloremque laboriosam?
         </p>
       </div>
     </div>
